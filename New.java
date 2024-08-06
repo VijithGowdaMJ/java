64ed9ded5ee2ebc69237bb8e153a5a8b9e88d1fa
VijithGowdaMJ <vijith.xworkz@gmail.com>
2024-08-06 17:03:35 +0530

    adding New file

---
+++ New.java
@@ -0,0 +1,17 @@
+public class New
+{
+	public static void main(String args[])
+	{
+		int count=0;
+		System.out.println(args.length);
+		for(int index=0; index<args.length; index++)
+		{
+			count++;
+			//System.out.println(args[index]);
+           if(args[index].equals("vijith"))
+		   {
+		   System.out.println("it is vijith");
+			}
+		}
+	}
+}
